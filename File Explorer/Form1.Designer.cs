a3e7626b6cea37034883d186d49cf7e0988ce86c
mkalme <mikelis.kalme@gmail.com>
2025-05-22 14:59:41 +0300

    Add project files.

---
+++ File Explorer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace File_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Image = new System.Windows.Forms.DataGridViewImageColumn();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastEdited = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.backButton = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Image,
+            this.FileName,
+            this.LastEdited,
+            this.Type,
+            this.Size});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 40);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(896, 442);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // Image
+            // 
+            this.Image.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Image.HeaderText = "";
+            this.Image.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Image.Name = "Image";
+            this.Image.ReadOnly = true;
+            this.Image.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Image.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Image.Width = 30;
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.FileName.HeaderText = "Name";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            this.FileName.Width = 538;
+            // 
+            // LastEdited
+            // 
+            this.LastEdited.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.LastEdited.HeaderText = "Last Edited";
+            this.LastEdited.Name = "LastEdited";
+            this.LastEdited.ReadOnly = true;
+            this.LastEdited.Width = 130;
+            // 
+            // Type
+            // 
+            this.Type.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            this.Type.Width = 95;
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "Size";
+            this.Size.Name = "Size";
+            this.Size.ReadOnly = true;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(12, 9);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(87, 25);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(114, 9);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(80, 25);
+            this.delete.TabIndex = 2;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 494);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "File Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.DataGridViewImageColumn Image;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastEdited;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ File Explorer/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace File_Explorer
+{
+    public partial class Form1 : Form
+    {
+        private String currentPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MODIFY DATAGRIDIVEW
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dataGridView1.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            //REFRESH DATAGRIDVIEW
+            driveInfo();
+        }
+
+        private void refreshDataGridView(){
+            try
+            {
+                String[] filePaths = Directory.GetFiles(currentPath);
+                String[] directoryPaths = Directory.GetDirectories(currentPath);
+
+                dataGridView1.Rows.Clear();
+
+                for (int i = 0; i < directoryPaths.Length; i++)
+                {
+                    dataGridView1.Rows.Add(Properties.Resources.folderImage, Path.GetFileName(directoryPaths[i]), Directory.GetLastWriteTime(directoryPaths[i]).ToString("dd.MM.yyyy  HH:mm"), "Directory", getSize(directoryPaths[i]));
+                }
+
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    long length = new FileInfo(filePaths[i]).Length;
+                    dataGridView1.Rows.Add(Properties.Resources.fileImage, Path.GetFileName(filePaths[i]), File.GetLastWriteTime(filePaths[i]).ToString("dd.MM.yyyy  HH:mm"), Path.GetExtension(filePaths[i]) + " File", getSize(filePaths[i]));
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            backButton.Enabled = true;
+            //dataGridView1.Sort(dataGridView1.Columns[1], ListSortDirection.Ascending);
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            if (!checkIfRoot())
+            {
+                currentPath = Directory.GetParent(currentPath).FullName;
+                refreshDataGridView();
+            }
+            else {
+                currentPath = "";
+                driveInfo();
+            }
+        }
+
+        private bool checkIfRoot() {
+            DirectoryInfo d = new DirectoryInfo(currentPath);
+            bool root = false;
+            if (d.Parent == null) {
+                root = true;
+            }
+            return root;
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            String fileName = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[1].Value.ToString();
+
+            if (!string.IsNullOrEmpty(currentPath))
+            {
+                Directory.Exists(currentPath + @"\" + fileName);
+
+                if (Directory.Exists(currentPath + @"\" + fileName))
+                {
+                    currentPath += @"\" + fileName;
+                    refreshDataGridView();
+                }
+            }
+            else {
+                currentPath = fileName;
+                refreshDataGridView();
+            }
+        }
+
+        private void driveInfo() {
+            DriveInfo[] filePaths = DriveInfo.GetDrives();
+
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                dataGridView1.Rows.Add(Properties.Resources.hardDriveImage, filePaths[i].Name, "", "Logical Drive", (filePaths[i].TotalSize / 1073741824).ToString() + " GB");
+            }
+
+            backButton.Enabled = false;
+        }
+
+        private String getSize(String path) {
+            String size = "";
+            double d = 0.0;
+
+            if (ifDirectory(path))
+            {
+                d = returnSize(path);
+            }
+            else{
+                d = new FileInfo(path).Length;
+            }
+
+            //Debug.WriteLine(d);
+
+            //int counting = 0;
+
+            /*for (int i = 0; i < 5; i++) {
+                if ((d / 1024.0) < 1)
+                {
+                    i = 5;
+                    break;
+                }
+                else {
+                    d /= 1024.0;
+                    counting++;
+                }
+            }*/
+
+            size = String.Format("{0:0}", d);
+
+            /*if (counting == 0 && d == 0)
+            {
+                size = "Empty";
+            }
+            else {
+                switch (counting)
+                {
+                    case 0:
+                        size += " Bytes";
+                        break;
+
+                    case 1:
+                        size += " KB";
+                        break;
+
+                    case 2:
+                        size += " MB";
+                        break;
+
+                    case 3:
+                        size += " GB";
+                        break;
+
+                    case 4:
+                        size += " TB";
+                        break;
+                }
+            }*/
+
+            return size;
+        }
+
+        private double returnSize(String path) {
+            double b = 0.0;
+
+            try
+            {
+                String[] filePaths = Directory.GetFiles(path);
+                String[] directoryPaths = Directory.GetDirectories(path);
+
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    b += new FileInfo(filePaths[i]).Length;
+                }
+
+                for (int i = 0; i < directoryPaths.Length; i++)
+                {
+                    b += returnSize(directoryPaths[i]);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return b;
+        }
+
+        private bool ifDirectory(String path) {
+            return Directory.Exists(path);
+        }
+
+    }
+}
